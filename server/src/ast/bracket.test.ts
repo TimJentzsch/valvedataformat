6d34e07324d80fc6d993bdd51bdf0ac1772cd289
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-08-22 20:43:43 +0200

    Add shorthand notation to create bracket node

---
+++ server/src/ast/bracket.test.ts
@@ -0,0 +1,60 @@
+import { getInlineRange, getRange } from "../parser/utils";
+import AstBracket, { astBracket, astLBracket, astRBracket } from "./bracket";
+
+describe('astBracket', () => {
+  test('should properly create left bracket node', () => {
+    const actual = astBracket("{", "left", getInlineRange(0, 0, 1));
+    const expected: AstBracket = {
+      type: "bracket",
+      children: [],
+      value: "{",
+      bracketType: "left",
+      range: getInlineRange(0, 0, 1),
+    };
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('should properly create right bracket node', () => {
+    const actual = astBracket("}", "right", getInlineRange(0, 0, 1));
+    const expected: AstBracket = {
+      type: "bracket",
+      children: [],
+      value: "}",
+      bracketType: "right",
+      range: getInlineRange(0, 0, 1),
+    };
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe('astLBracket', () => {
+  test('should properly create left bracket node', () => {
+    const actual = astLBracket(getInlineRange(0, 0, 1));
+    const expected: AstBracket = {
+      type: "bracket",
+      children: [],
+      value: "{",
+      bracketType: "left",
+      range: getInlineRange(0, 0, 1),
+    };
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe('astRBracket', () => {
+  test('should properly create right bracket node', () => {
+    const actual = astRBracket(getInlineRange(0, 0, 1));
+    const expected: AstBracket = {
+      type: "bracket",
+      children: [],
+      value: "}",
+      bracketType: "right",
+      range: getInlineRange(0, 0, 1),
+    };
+
+    expect(actual).toEqual(expected);
+  });
+});
--- server/src/ast/bracket.ts
+++ server/src/ast/bracket.ts
@@ -1,9 +1,38 @@
+import { Range } from "vscode-languageserver/node";
 import AstNode from "./node";
+
+export type BracketType = "left" | "right";
 
 /** A bracket for an object. */
 export default interface AstBracket extends AstNode {
   type: "bracket";
   /** Left is an opening bracket, right is a closing bracket. */
-  bracketType: "left" | "right";
+  bracketType: BracketType;
+  value: string;
   children: [];
+}
+
+/** Create an AST node for a bracket. */
+export function astBracket(
+  value: string,
+  bracketType: BracketType,
+  range: Range
+): AstBracket {
+  return {
+    type: "bracket",
+    children: [],
+    value,
+    bracketType,
+    range,
+  };
+}
+
+/** Create an AST node for a left bracket ({). */
+export function astLBracket(range: Range): AstBracket {
+  return astBracket("{", "left", range);
+}
+
+/** Create an AST node for a right bracket (}). */
+export function astRBracket(range: Range): AstBracket {
+  return astBracket("}", "right", range);
 }

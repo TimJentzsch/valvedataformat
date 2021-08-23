28c743f9cc6b448671c9a4b7aa3df7bf408052fb
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-08-23 15:22:06 +0200

    Add more tests for string property parsing Add tests for key-value pairs without indent in-between and for values without the closing quotes.

--- server/src/parser/property.test.ts
+++ server/src/parser/property.test.ts
@@ -7,17 +7,30 @@ import { applyParser, getInlineRange } from "./utils";
 
 // String properties
 describe("should parse string properties", () => {
+  const input1 = "key value";
   const key1 = astUnquotedKey("key", getInlineRange(0, 0, 3));
   const indent1 = astIndent(" ", getInlineRange(0, 3, 4));
   const value1 = astUnquotedString("value", getInlineRange(0, 4, 9));
 
+  const input2 = '"key" "value"';
   const key2 = astQuotedKey(true, "key", getInlineRange(0, 0, 5));
   const indent2 = astIndent(" ", getInlineRange(0, 5, 6));
   const value2 = astQuotedString(true, "value", getInlineRange(0, 6, 13));
 
+  const input3 = '"key""value"';
+  const key3 = astQuotedKey(true, "key", getInlineRange(0, 0, 5));
+  const value3 = astQuotedString(true, "value", getInlineRange(0, 5, 12));
+
+  const input4 = '"key" "value';
+  const key4 = astQuotedKey(true, "key", getInlineRange(0, 0, 5));
+  const indent4 = astIndent(" ", getInlineRange(0, 5, 6));
+  const value4 = astQuotedString(false, "value", getInlineRange(0, 6, 12));
+
   const params: Array<[string, AstProperty]> = [
-    ["key value", astProperty([key1, indent1, value1], key1, value1)],
-    ['"key" "value"', astProperty([key2, indent2, value2], key2, value2)],
+    [input1, astProperty([key1, indent1, value1], key1, value1)],
+    [input2, astProperty([key2, indent2, value2], key2, value2)],
+    [input3, astProperty([key3, value3], key3, value3)],
+    [input4, astProperty([key4, indent4, value4], key4, value4)],
   ];
 
   for (const [input, expected] of params) {

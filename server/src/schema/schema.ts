e01fb06c1d15388c88e9c50eee4fe6f35a5273f1
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-09-12 13:33:46 +0200

    Add interface for string schema

--- server/src/schema/schema.ts
+++ server/src/schema/schema.ts
@@ -34,3 +34,18 @@ interface SchemaObject extends VdfSchemaBase {
   /** The maximum number of properties. */
   maxProperties?: number;
 }
+
+/** A VDF string.
+ * @see https://json-schema.org/understanding-json-schema/reference/string.html
+ */
+interface SchemaString extends VdfSchemaBase {
+  type: "string";
+  /** The minimum length of the string. */
+  minLength?: number;
+  /** The maximum length of the string. */
+  maxLength?: number;
+  /** The pattern the string has to match. */
+  pattern?: string;
+  /** The format of the string. */
+  format?: string;
+}

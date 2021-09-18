39eb60c831e4f8ec9320077cd7737623e180bd13
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-09-18 16:16:46 +0200

    Add tests for string schema attributes

--- server/src/test/schema/schemaValidation.test.ts
+++ server/src/test/schema/schemaValidation.test.ts
@@ -49,8 +49,34 @@ describe("validateStringSchema", () => {
   // The name of the test, the node to validate, the expected diagnostic ranges
   const params: Array<[string, AstNode, Range[]]> = [
     [
-      "should return no errors for basic string",
+      "should return no errors for basic schema",
       astUnquotedString("content", getInlineRange(0, 0, 7), { type: "string" }),
+      [],
+    ],
+    [
+      "should return no errors for valid complex schema",
+      astUnquotedString("content", getInlineRange(0, 0, 7), {
+        type: "string",
+        pattern: "^\\w+$",
+        minLength: 2,
+        maxLength: 20,
+      }),
+      [],
+    ],
+    [
+      "should return no errors for valid const schema",
+      astUnquotedString("content", getInlineRange(0, 0, 7), {
+        type: "string",
+        const: "content",
+      }),
+      [],
+    ],
+    [
+      "should return no errors for valid enum schema",
+      astUnquotedString("content", getInlineRange(0, 0, 7), {
+        type: "string",
+        enum: ["foo", "content", "bar"],
+      }),
       [],
     ],
     [
@@ -67,6 +93,46 @@ describe("validateStringSchema", () => {
         { type: "string" }
       ),
       [getInlineRange(0, 0, 2)],
+    ],
+    [
+      "should return error for const violation",
+      astUnquotedString("content", getInlineRange(0, 0, 7), {
+        type: "string",
+        const: "foo",
+      }),
+      [getInlineRange(0, 0, 7)],
+    ],
+    [
+      "should return error for enum violation",
+      astUnquotedString("content", getInlineRange(0, 0, 7), {
+        type: "string",
+        enum: ["foo", "bar"],
+      }),
+      [getInlineRange(0, 0, 7)],
+    ],
+    [
+      "should return error for minLength violation",
+      astUnquotedString("content", getInlineRange(0, 0, 7), {
+        type: "string",
+        minLength: 20,
+      }),
+      [getInlineRange(0, 0, 7)],
+    ],
+    [
+      "should return error for maxLength violation",
+      astUnquotedString("content", getInlineRange(0, 0, 7), {
+        type: "string",
+        maxLength: 3,
+      }),
+      [getInlineRange(0, 0, 7)],
+    ],
+    [
+      "should return error for pattern violation",
+      astUnquotedString("content", getInlineRange(0, 0, 7), {
+        type: "string",
+        pattern: "\\d+",
+      }),
+      [getInlineRange(0, 0, 7)],
     ],
   ];
 

98b516d202378f9f263eb47495b80f7586a66ffa
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-09-19 14:53:33 +0200

    Add tests for number schema validation

--- server/src/test/schema/validateNumberSchema.test.ts
+++ server/src/test/schema/validateNumberSchema.test.ts
@@ -6,48 +6,55 @@ import { astQuotedString, astUnquotedString } from "../../ast/string";
 import { getInlineRange } from "../../parser/utils";
 import validateNodeSchema from "../../schema/schemaValidation";
 
-describe("validateIntegerSchema", () => {
+describe("validateNumberSchema", () => {
   // The name of the test, the node to validate, the expected diagnostic ranges
   const params: Array<[string, AstNode, Range[]]> = [
     [
       "should return no errors for 0",
       astUnquotedString("0", getInlineRange(0, 0, 1), {
-        type: "integer",
+        type: "number",
       }),
       [],
     ],
     [
       "should return no errors for 1",
       astUnquotedString("1", getInlineRange(0, 0, 1), {
-        type: "integer",
+        type: "number",
       }),
       [],
     ],
     [
       "should return no errors for 2561",
       astUnquotedString("2561", getInlineRange(0, 0, 4), {
-        type: "integer",
+        type: "number",
       }),
       [],
     ],
     [
       "should return no errors for 364.0",
       astUnquotedString("364.0", getInlineRange(0, 0, 5), {
-        type: "integer",
+        type: "number",
       }),
       [],
     ],
     [
-      "should return error for 2.3",
+      "should return no errors for 2.3",
       astUnquotedString("2.3", getInlineRange(0, 0, 3), {
-        type: "integer",
+        type: "number",
       }),
-      [getInlineRange(0, 0, 3)],
+      [],
+    ],
+    [
+      "should return no errors for 376.3123198",
+      astUnquotedString("376.3123198", getInlineRange(0, 0, 11), {
+        type: "number",
+      }),
+      [],
     ],
     [
       "should return no errors for valid complex schema",
-      astUnquotedString("23", getInlineRange(0, 0, 2), {
-        type: "integer",
+      astUnquotedString("23.0", getInlineRange(0, 0, 4), {
+        type: "number",
         minimum: 23,
         exclusiveMinimum: 22,
         maximum: 23,
@@ -57,36 +64,36 @@ describe("validateIntegerSchema", () => {
     ],
     [
       "should return no errors for valid const schema",
-      astUnquotedString("3", getInlineRange(0, 0, 1), {
-        type: "integer",
-        const: 3,
+      astUnquotedString("3.2", getInlineRange(0, 0, 3), {
+        type: "number",
+        const: 3.2,
       }),
       [],
     ],
     [
       "should return no errors for valid enum schema",
-      astUnquotedString("3", getInlineRange(0, 0, 1), {
-        type: "integer",
-        enum: [2, 7, 3],
+      astUnquotedString("3.2", getInlineRange(0, 0, 3), {
+        type: "number",
+        enum: [2.8, 7.0, 3.2],
       }),
       [],
     ],
     [
       "should return error for empty string",
-      astQuotedString("", getInlineRange(0, 0, 2), true, { type: "integer" }),
+      astQuotedString("", getInlineRange(0, 0, 2), true, { type: "number" }),
       [getInlineRange(0, 0, 2)],
     ],
     [
       "should return error for content string",
       astQuotedString("content", getInlineRange(0, 0, 9), true, {
-        type: "integer",
+        type: "number",
       }),
       [getInlineRange(0, 0, 9)],
     ],
     [
       "should return error for partial int",
       astQuotedString("34a", getInlineRange(0, 0, 4), true, {
-        type: "integer",
+        type: "number",
       }),
       [getInlineRange(0, 0, 4)],
     ],
@@ -96,57 +103,57 @@ describe("validateIntegerSchema", () => {
         astLBracket(getInlineRange(0, 0, 1)),
         [],
         astRBracket(getInlineRange(0, 1, 2)),
-        { type: "integer" }
+        { type: "number" }
       ),
       [getInlineRange(0, 0, 2)],
     ],
     [
       "should return error for const violation",
-      astUnquotedString("4", getInlineRange(0, 0, 1), {
-        type: "integer",
-        const: 3,
+      astUnquotedString("4.3", getInlineRange(0, 0, 3), {
+        type: "number",
+        const: 3.8,
       }),
-      [getInlineRange(0, 0, 1)],
+      [getInlineRange(0, 0, 3)],
     ],
     [
       "should return error for enum violation",
-      astUnquotedString("7", getInlineRange(0, 0, 1), {
-        type: "integer",
-        enum: [3, 2, 100],
+      astUnquotedString("7.4", getInlineRange(0, 0, 3), {
+        type: "number",
+        enum: [3.9, 2.1, 7.2, 100.6789],
       }),
-      [getInlineRange(0, 0, 1)],
+      [getInlineRange(0, 0, 3)],
     ],
     [
       "should return error for minimum violation",
-      astUnquotedString("23", getInlineRange(0, 0, 2), {
-        type: "integer",
-        minimum: 24,
+      astUnquotedString("23.1", getInlineRange(0, 0, 4), {
+        type: "number",
+        minimum: 23.11,
       }),
-      [getInlineRange(0, 0, 2)],
+      [getInlineRange(0, 0, 4)],
     ],
     [
       "should return error for maximum violation",
-      astUnquotedString("23", getInlineRange(0, 0, 2), {
-        type: "integer",
-        maximum: 22,
+      astUnquotedString("23.1", getInlineRange(0, 0, 4), {
+        type: "number",
+        maximum: 23.09,
       }),
-      [getInlineRange(0, 0, 2)],
+      [getInlineRange(0, 0, 4)],
     ],
     [
       "should return error for exclusive minimum violation",
-      astUnquotedString("23", getInlineRange(0, 0, 2), {
-        type: "integer",
-        exclusiveMinimum: 23,
+      astUnquotedString("23.1", getInlineRange(0, 0, 4), {
+        type: "number",
+        exclusiveMinimum: 23.1,
       }),
-      [getInlineRange(0, 0, 2)],
+      [getInlineRange(0, 0, 4)],
     ],
     [
       "should return error for exclusive maximum violation",
-      astUnquotedString("23", getInlineRange(0, 0, 2), {
-        type: "integer",
-        exclusiveMaxmimum: 23,
+      astUnquotedString("23.1", getInlineRange(0, 0, 4), {
+        type: "number",
+        exclusiveMaxmimum: 23.1,
       }),
-      [getInlineRange(0, 0, 2)],
+      [getInlineRange(0, 0, 4)],
     ],
   ];
 

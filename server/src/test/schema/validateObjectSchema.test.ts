8cbe4a57996dc7dc3f900f35694fd78337dbf5a1
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-09-19 17:53:51 +0200

    Add basic tests for object schema validation

--- server/src/test/schema/validateObjectSchema.test.ts
+++ server/src/test/schema/validateObjectSchema.test.ts
@@ -1,25 +1,27 @@
 import { Range } from "vscode-languageserver/node";
+import { astLBracket, astRBracket } from "../../ast/bracket";
 import AstNode from "../../ast/node";
-import { astQuotedString, astUnquotedString } from "../../ast/string";
+import { astObject } from "../../ast/object";
+import { astUnquotedString } from "../../ast/string";
 import { getInlineRange } from "../../parser/utils";
 import validateNodeSchema from "../../schema/schemaValidation";
 
-describe("validateNullSchema", () => {
+describe("validateObjectSchema", () => {
   // The name of the test, the node to validate, the expected diagnostic ranges
   const params: Array<[string, AstNode, Range[]]> = [
     [
-      "should return no errors for null string",
-      astQuotedString("", getInlineRange(0, 0, 2), true, { type: "null" }),
+      "should return no errors for valid empty object",
+      astObject(
+        astLBracket(getInlineRange(0, 0, 1)),
+        [],
+        astRBracket(getInlineRange(0, 1, 2)),
+        { type: "object" }
+      ),
       [],
     ],
     [
-      "should return error for boolean string",
-      astUnquotedString("0", getInlineRange(0, 0, 1), { type: "null" }),
-      [getInlineRange(0, 0, 1)],
-    ],
-    [
-      "should return error for content string",
-      astUnquotedString("content", getInlineRange(0, 0, 7), { type: "null" }),
+      "should return error for string",
+      astUnquotedString("content", getInlineRange(0, 0, 7), { type: "object" }),
       [getInlineRange(0, 0, 7)],
     ],
   ];
